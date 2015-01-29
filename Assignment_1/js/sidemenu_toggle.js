4efa494d4ef3caf2d3a5935f8be0826528fef85f
Rhett Amin <rhettamin@gmail.com>
2015-01-29 04:01:10 -0500

    Added materializecss and update contact.html

---
+++ Assignment_1/js/sidemenu_toggle.js
@@ -0,0 +1,32 @@
+(function() {
+	var body = $('body');
+	$('#toggle-sidemenu').bind('click', function() {
+		body.toggleClass('sidemenu-open');
+		return false;
+	});
+
+	$('.content').click(function(){
+		if(body.hasClass('sidemenu-open')){
+			body.toggleClass('sidemenu-open');
+			return false;
+		}
+	});
+
+	$('.page-footer').click(function(){
+		if(body.hasClass('sidemenu-open')){
+			body.toggleClass('sidemenu-open');
+			return false;
+		}
+	});
+
+	/* Delay anchor tag href links*/
+	$('a').click(function(e) {
+	    var anchor = $(this), h;
+	    h = anchor.attr('href');
+	    e.preventDefault();
+	    anchor.animate({'opacity' : 50}, 200, function() {
+	        window.location = h;
+	    });
+	});
+
+})();
