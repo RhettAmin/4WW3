4d03442a834fecc287f1a5c2508891cffa7d5257
Rhett Amin <rhettamin@gmail.com>
2015-02-18 01:51:12 -0500

    commented jquery code

--- Assignment_2/js/login.js
+++ Assignment_2/js/login.js
@@ -1,24 +1,36 @@
 $(document).ready(function(){
 
-	for (var i = 0; i < localStorage.length; i++) {
-		console.log(JSON.parse(localStorage.getItem().key(i));)
-	}
-
+	/*********************************************
+	This function is called when the user clicks 
+	the Submit button. It checks check if both 
+	fields have values then determines if those 
+	values are correct with what is stored in
+	localstorage. 
+	*********************************************/
 	$("#submit_log").click(function(e) {
-		e.preventDefault();
-
-		if (localStorage.getItem("user_"+$("#icon_uname").val()) === null) {
-			toast("That Username doesn't exist" , 2000);
-		} else {
-			var check = JSON.parse(localStorage.getItem("user_"+$("#icon_uname").val()));
-			if ($("#icon_uname").val()==check.username && $("#icon_pwrd").val()==check.password){
-				toast("Welcome " + $("#icon_uname").val() + "!" , 2000);
-				setTimeout(function(){ window.location = "index.html"; }, 2000);
-				$('#regis_form').trigger("reset");
-			} else if ($("#icon_pwrd").val()!=check.password) {
-				toast("Password does not match" , 2000);
+		e.preventDefault(); // stops page from refreshing
+		
+		if ($("#icon_uname").val() == "" && $("#icon_pwrd").val() == "") { 	/* Check if the fields are not empty */
+			toast("Fields are empty" , 2000);
+		} else if(!$("#icon_uname").val() && $("#icon_pwrd").val()) { 		/* Check if username is not empty */
+			toast("No username entered" , 2000);
+		} else if($("#icon_uname").val() && !$("#icon_pwrd").val()) { 		/* Check if passowrd is not empty */
+			toast("No password entered" , 2000);
+		} else {												
+			if (localStorage.getItem("user_"+$("#icon_uname").val()) === null) { /* Check if usernmae exists in storage */
+				toast("That Username doesn't exist" , 2000);
+			} else {
+				var check = JSON.parse(localStorage.getItem("user_"+$("#icon_uname").val()));		//get key with entered credentials
+				/* Check if credentials are correct, welcome then load index page */
+				if ($("#icon_uname").val()==check.username && $("#icon_pwrd").val()==check.password){ 
+					toast("Welcome " + $("#icon_uname").val() + "!" , 1500);						//Welcome user
+					setTimeout(function(){ window.location = "index.html"; }, 1500);
+					$('#loging_form').trigger("reset");
+				} else if ($("#icon_pwrd").val()!=check.password) { /* usernmae is right, but password isnt*/
+					toast("Password does not match" , 2000);
+				}
 			}
 		}
-	});
+	});			//End click function
 
 });
--- Assignment_2/js/sidemenu_toggle.js
+++ Assignment_2/js/sidemenu_toggle.js
@@ -1,10 +1,12 @@
 $(document).ready(function () {
 	var body = $('body');
+	//Open the sidemenu when user clicks hamburger icon
 	$('#toggle-sidemenu').bind('click', function() {
 		body.toggleClass('sidemenu-open');
 		return false;
 	});
 
+	//Close sidemenu when the body is clicked
 	$('.content').click(function(){
 		if(body.hasClass('sidemenu-open')){
 			body.toggleClass('sidemenu-open');
@@ -12,6 +14,7 @@ $(document).ready(function () {
 		}
 	});
 
+	//Close sidemenu when the footer is clicked
 	$('.page-footer').click(function(){
 		if(body.hasClass('sidemenu-open')){
 			body.toggleClass('sidemenu-open');
@@ -19,7 +22,7 @@ $(document).ready(function () {
 		}
 	});
 
-	/* Delay anchor tag href links*/
+	//sidemenu, chaning pages on click
 	$('a').click(function(e) {
 	    var h = $(this).attr('href');
 	    e.preventDefault();
