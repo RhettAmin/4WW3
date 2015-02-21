1b06b493261ea50683c672dd4e66c63d26d8081e
jamesbanthony <jamesbanthony@users.noreply.github.com>
2015-02-21 18:07:11 -0500

    Update reviews.js
    added comments

--- Assignment_2/js/reviews.js
+++ Assignment_2/js/reviews.js
@@ -1,3 +1,7 @@
+/* This function takes text that the user enters into the text area and a rating from the slider.
+ * When the submit button is pressed this function prepends new code to the reviews html file.
+ * The code is created with the text entered by the user and the rating they gave using the slider.
+ * Each review will appear under the review submission area as a new card, with the most recent ratings appearing first. */
 $(document).ready(function(){
 
 	$("#submit").click(function(e){
